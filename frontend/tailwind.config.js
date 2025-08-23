09b6d4b771104c73e5372b82784dea4ee514f8c3
Alfredo / Maplide / RussP <russplayadaqu666@gmail.com>
2025-08-22 23:02:19 -0500

    Mejorando la base del estilo

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // sin darkMode
   content: ['./index.html','./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: '#2563eb', // azul
-        accent:  '#fbbf24', // amarillo
+        primary: '#2563eb',   // azul
+        accent:  '#fbbf24',   // amarillo
       },
       borderRadius: { '2xl': '1rem' },
     },
