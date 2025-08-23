f54263b52dbc7041390577ede334ed2e82ab2ca3
Alfredo / Maplide / RussP <russplayadaqu666@gmail.com>
2025-08-22 23:02:19 -0500

    Mejorando la base del estilo

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,10 +1,6 @@
-// postcss.config.cjs (CommonJS)
 const tailwind = require('@tailwindcss/postcss');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  plugins: [
-    tailwind({ config: './tailwind.config.js' }), // <- forzado
-    autoprefixer(),
-  ],
+  plugins: [tailwind(), autoprefixer()],
 };
